f5ac9fb72bca684f73b37b9c4dd752f5efd57b11
Woen8 <woenof@gmail.com>
2020-08-04 10:06:44 -0300

    ConfiguredServer

---
+++ server/src/app.ts
@@ -0,0 +1,22 @@
+import routes from "./routes";
+import express, { Express } from "express";
+
+class App {
+  public server: Express;
+
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
---
+++ web/src/pages/TeachersForm/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PageHeader from "../../components/PageHeader";
+
+const TeachersForm = () => {
+  return <PageHeader title="Que incrível que você quer dar aulas"></PageHeader>;
+};
+
+export default TeachersForm;
